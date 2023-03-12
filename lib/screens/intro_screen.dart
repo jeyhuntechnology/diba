fdcdc460a248459f33b1ffea9938eda7a5911a39
hamid313-coder <hamid.hussainy0@gmail.com>
2023-03-12 15:31:18 +0430

    added const for optimization

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -9,92 +9,96 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF9e777b),
+        backgroundColor: const Color(0xFF9e777b),
         body: SafeArea(
-          
-      child: IntroductionScreen(
-
-        globalBackgroundColor: Colors.brown[300],
-        scrollPhysics: BouncingScrollPhysics(),
-        pages: [
-          PageViewModel(
-            titleWidget: Text(
-              'دیبا',
+          child: IntroductionScreen(
+            globalBackgroundColor: Colors.brown[300],
+            scrollPhysics: const BouncingScrollPhysics(),
+            pages: [
+              PageViewModel(
+                titleWidget: const Text(
+                  'دیبا',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                body: 'به اپلیکشن دیبا خوش آمدید',
+                image: const CircleAvatar(
+                  backgroundImage: AssetImage('images/1.jpg'),
+                  radius: 130,
+                ),
+              ),
+              PageViewModel(
+                titleWidget: const Text(
+                  'مدیریت اندازه گیری',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                body:
+                    '    دیگر برای اندازه گیری لباس نیازی نیست دنبال دفتر بگردید شما میتوانید با داشتن اپلیکشن خیاطی دیبا اندازه ها را د گوشی همراه خود داشته باشید و همیشه همراه تان باشد',
+                image: const CircleAvatar(
+                  backgroundImage: AssetImage('images/2.jpg'),
+                  radius: 130,
+                ),
+              ),
+              PageViewModel(
+                titleWidget: const Text(
+                  'اندازه گیری لباس ها',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                body:
+                    ' دیگر برای اندازه گیری لباس نیازی نیست دنبال دفتر بگردید شما میتوانید با داشتن اپلیکشن خیاطی دیبا اندازه ها را د گوشی همراه خود داشته باشید و همیشه همراه تان باشد   ',
+                image: const CircleAvatar(
+                  backgroundImage: AssetImage('images/1.jpg'),
+                  radius: 130,
+                ),
+              ),
+            ],
+            onDone: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Register(),
+                ),
+              );
+            },
+            onSkip: () {},
+            showSkipButton: true,
+            skip: const Text(
+              "skip",
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black),
+            ),
+            next: const Icon(
+              Icons.arrow_forward,
+              color: Colors.grey,
+            ),
+            done: const Text(
+              "Done",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black),
+            ),
+            dotsDecorator: DotsDecorator(
+              size: const Size.square(10.0),
+              activeSize: const Size(20.0, 10.0),
+              color: Colors.black,
+              activeColor: Colors.grey,
+              spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
               ),
             ),
-            body: 'به اپلیکشن دیبا خوش آمدید',
-            image: CircleAvatar(
-              backgroundImage: AssetImage('images/1.jpg'),
-              radius: 130,
-            ),
           ),
-          PageViewModel(
-            titleWidget: Text(
-              'مدیریت اندازه گیری',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-            body:
-                '    دیگر برای اندازه گیری لباس نیازی نیست دنبال دفتر بگردید شما میتوانید با داشتن اپلیکشن خیاطی دیبا اندازه ها را د گوشی همراه خود داشته باشید و همیشه همراه تان باشد',
-            image: CircleAvatar(
-              backgroundImage: AssetImage('images/2.jpg'),
-              radius: 130,
-            ),
-          ),
-          PageViewModel(
-            titleWidget: Text(
-              'اندازه گیری لباس ها',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-            body:
-                ' دیگر برای اندازه گیری لباس نیازی نیست دنبال دفتر بگردید شما میتوانید با داشتن اپلیکشن خیاطی دیبا اندازه ها را د گوشی همراه خود داشته باشید و همیشه همراه تان باشد   ',
-            image: CircleAvatar(
-              backgroundImage: AssetImage('images/1.jpg'),
-              radius: 130,
-            ),
-          ),
-        ],
-        onDone: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Register(),
-            ),
-          );
-        },
-        onSkip: () {},
-        showSkipButton: true,
-        skip: Text(
-          "skip",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18,color:Colors.black),
-        ),
-        next: Icon(
-          Icons.arrow_forward,
-          color: Colors.grey,
-        ),
-        done: Text(
-          "Done",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18,color: Colors.black),
-        ),
-        dotsDecorator: DotsDecorator(
-          size: Size.square(10.0),
-          activeSize: Size(20.0, 10.0),
-          color: Colors.black,
-          activeColor: Colors.grey,
-          spacing: EdgeInsets.symmetric(horizontal: 3.0),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25.0),
-          ),
-        ),
-      ),
-    ));
+        ));
   }
 }
